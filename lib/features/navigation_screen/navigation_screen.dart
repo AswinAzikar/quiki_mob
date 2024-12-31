531f8e1d81bc9ff711f0624eb4c616199f2d74fc
Aswinazikar <azwinaswin8@gmail.com>
2024-12-31 08:58:05 +0530

    basic nav settings implemented

--- lib/features/navigation_screen/navigation_screen.dart
+++ lib/features/navigation_screen/navigation_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+import 'package:quiki/constants/constant.dart';
 
 class NavigationScreen extends StatefulWidget {
   const NavigationScreen({super.key});
@@ -8,8 +10,62 @@ class NavigationScreen extends StatefulWidget {
 }
 
 class _NavigationScreenState extends State<NavigationScreen> {
+  late PersistentTabController _tabcontroller;
+  final bool _hideNavbar = true;
+
+  final NavBarStyle _navBarStyle = NavBarStyle.style3;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabcontroller = PersistentTabController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  final List<PersistentBottomNavBarItem> _navBarItems = [
+    PersistentBottomNavBarItem(
+        icon: Icon(Icons.home),
+        title: "Home",
+        activeColorPrimary: primaryColor,
+        inactiveColorPrimary: darkGreyColor),
+    PersistentBottomNavBarItem(
+        icon: Icon(Icons.people),
+        title: "user",
+        activeColorPrimary: primaryColor,
+        inactiveColorPrimary: darkGreyColor),
+    PersistentBottomNavBarItem(
+        icon: Icon(Icons.settings),
+        title: "Settings",
+        activeColorPrimary: primaryColor,
+        inactiveColorPrimary: darkGreyColor)
+  ];
+
+  final List<Widget> _screens = [
+    Center(
+      child: Text("Home"),
+    ),
+    Center(
+      child: Text("users"),
+    ),
+    Center(
+      child: Text("settings"),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: PersistentTabView(
+        navBarStyle: _navBarStyle,
+        context,
+        screens: _screens,
+        items: _navBarItems,
+        stateManagement: true,
+      ),
+    );
   }
 }
